b6fe36835b08c6796c59c9005fe9f3d1bd39752a
David Reeves <djreeves2005@gmail.com>
2020-06-18 17:00:05 -0400

    new publish

---
+++ LightScout/LightScout.Android/obj/Debug/90/android/typemap.mj.x86_64.s
@@ -0,0 +1,25 @@
+	/* Data Hash: E09805367A73450C */
+	.file	"typemap.mj.inc"
+
+	/* Mapping header */
+	.section	.data.mj_typemap,"aw",@progbits
+	.type	mj_typemap_header, @object
+	.p2align	2
+	.global	mj_typemap_header
+mj_typemap_header:
+	/* version */
+	.long	1
+	/* entry-count */
+	.long	7180
+	/* entry-length */
+	.long	266
+	/* value-offset */
+	.long	145
+	.size	mj_typemap_header, 16
+
+	/* Mapping data */
+	.type	mj_typemap, @object
+	.global	mj_typemap
+mj_typemap:
+	.size	mj_typemap, 1909881
+	.include	"typemap.mj.inc"

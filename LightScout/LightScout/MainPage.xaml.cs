916f5ec8de5fc853ac930f9e815611d25c595120
David Reeves <djreeves2005@gmail.com>
2020-06-19 10:04:04 -0400

    The new library doesn't work. Thinking of having the server side on NodeJS instead of a C# library

--- LightScout/LightScout/MainPage.xaml.cs
+++ LightScout/LightScout/MainPage.xaml.cs
@@ -1,5 +1,8 @@
 ﻿using LightScout.Models;
-using Plugin.BluetoothLE;
+using Plugin.BLE;
+using Plugin.BLE.Abstractions;
+using Plugin.BLE.Abstractions.Contracts;
+using Plugin.BLE.Abstractions.EventArgs;
 using System;
 using System.Collections.Generic;
 using System.Collections.ObjectModel;
@@ -17,6 +20,8 @@ namespace LightScout
     public partial class MainPage : ContentPage
     {
         private static bool[] ControlPanel = new bool[2];
+        private static IBluetoothLE ble = CrossBluetoothLE.Current;
+        private static IAdapter adapter = CrossBluetoothLE.Current.Adapter;
         private static IDevice deviceIWant;
         private static bool Balanced;
         private static int BluetoothDevices = 0;
@@ -26,6 +31,35 @@ namespace LightScout
             InitializeComponent();
             ControlPanel[0] = false;
             ControlPanel[1] = false;
+            adapter.DeviceDiscovered += async (s, a) =>
+            {
+
+                BluetoothDevices++;
+                if (a.Device.Name != null)
+                {
+                    Devices.Add(a.Device);
+                }
+                listofdevices.ItemsSource = Devices;
+
+            };
+            adapter.DeviceConnected += async (s, a) =>
+            {
+                Console.WriteLine("Connected to: " + a.Device.Name.ToString());
+                BluetoothFind.Text = "Connected to: " + a.Device.Name.ToString();
+                deviceIWant = a.Device;
+                var services = await deviceIWant.GetServicesAsync();
+                Console.WriteLine("Current Services: " + services.ToString());
+            };
+            adapter.DeviceConnectionLost += (s, a) =>
+            {
+                Console.WriteLine("Lost connection to: " + a.Device.Name.ToString());
+                BluetoothFind.Text = "Disconnected from: " + a.Device.Name.ToString();
+            };
+            adapter.DeviceDisconnected += (s, a) =>
+            {
+                Console.WriteLine("Lost connection to: " + a.Device.Name.ToString());
+                BluetoothFind.Text = "Disconnected from: " + a.Device.Name.ToString();
+            };
         }
 
         private void FTCShow_Clicked(object sender, EventArgs e)
@@ -40,51 +74,26 @@ namespace LightScout
         private async void CheckBluetooth(object sender, EventArgs e)
         {
             //BindingContext = new BluetoothDeviceViewModel();
-
-            CrossBleAdapter.Current.Scan().Subscribe(scanResult => {
-                Devices.Add(scanResult.Device);
-                listofdevices.ItemsSource = Devices;
-            });
-
             
-            /*Devices.Clear();
-            adapter.DeviceDiscovered += async (s, a) =>
-            {
-
-                BluetoothDevices++;
-                if (a.Device.Name != null)
-                {
-                    Devices.Add(a.Device.Name);
-                }
-                listofdevices.ItemsSource = Devices;
-                deviceIWant = a.Device;
-
-            };
-            adapter.DeviceConnected += (s, a) =>
-            {
-                Console.WriteLine("Connected to: " + a.Device.Name.ToString());
-            };
-            adapter.DeviceConnectionLost += (s, a) =>
-            {
-                Console.WriteLine("Lost connection to: " + a.Device.Name.ToString());
-            };
-            adapter.DeviceDisconnected += (s, a) =>
-            {
-                Console.WriteLine("Lost connection to: " + a.Device.Name.ToString());
-            };
-            await adapter.StartScanningForDevicesAsync();*/
+            Devices.Clear();
+            
+            await adapter.StartScanningForDevicesAsync();
             
         }
 
-        private void listofdevices_ItemTapped(object sender, ItemTappedEventArgs e)
+        private async void listofdevices_ItemTapped(object sender, ItemTappedEventArgs e)
         {
             IDevice selectedDevice = e.Item as IDevice;
-            selectedDevice.Connect();
             
-            selectedDevice.WhenConnected().Subscribe(t =>
+            if(deviceIWant != null)
             {
-                Console.WriteLine("BLE CUSTOM ::: Successfully connected to " + t.Name);
-            });
+                await adapter.DisconnectDeviceAsync(deviceIWant);
+                await adapter.ConnectToDeviceAsync(selectedDevice);
+            }
+            else
+            {
+                await adapter.ConnectToDeviceAsync(selectedDevice);
+            }
         }
     }
 }

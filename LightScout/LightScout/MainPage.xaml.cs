ec18e54940299f346bd0ef61ae6e8b3001d140b1
David Reeves <djreeves2005@gmail.com>
2020-06-21 20:14:22 -0400

    new bluetooth test page

--- LightScout/LightScout/MainPage.xaml.cs
+++ LightScout/LightScout/MainPage.xaml.cs
@@ -20,52 +20,16 @@ namespace LightScout
     public partial class MainPage : ContentPage
     {
         private static bool[] ControlPanel = new bool[2];
-        private static IBluetoothLE ble = CrossBluetoothLE.Current;
-        private static IAdapter adapter = CrossBluetoothLE.Current.Adapter;
-        private static IDevice deviceIWant;
+        
         private static bool Balanced;
         private static int BluetoothDevices = 0;
-        private static ObservableCollection<IDevice> Devices = new ObservableCollection<IDevice>();
+        
         public MainPage()
         {
             InitializeComponent();
             ControlPanel[0] = false;
             ControlPanel[1] = false;
-            adapter.DeviceDiscovered += async (s, a) =>
-            {
-
-                BluetoothDevices++;
-                if (a.Device.Name != null)
-                {
-                    Devices.Add(a.Device);
-                }
-                listofdevices.ItemsSource = Devices;
-
-            };
-            adapter.DeviceConnected += async (s, a) =>
-            {
-                Console.WriteLine("Connected to: " + a.Device.Name.ToString());
-                BluetoothFind.Text = "Connected to: " + a.Device.Name.ToString();
-                deviceIWant = a.Device;
-                var services = await deviceIWant.GetServicesAsync();
-                Console.WriteLine("Current Services: " + services.ToString());
-                listofdevices.IsVisible = false;
-                sendDataToBT.IsVisible = true;
-            };
-            adapter.DeviceConnectionLost += (s, a) =>
-            {
-                Console.WriteLine("Lost connection to: " + a.Device.Name.ToString());
-                BluetoothFind.Text = "Disconnected from: " + a.Device.Name.ToString();
-                listofdevices.IsVisible = true;
-                sendDataToBT.IsVisible = false;
-            };
-            adapter.DeviceDisconnected += (s, a) =>
-            {
-                Console.WriteLine("Lost connection to: " + a.Device.Name.ToString());
-                BluetoothFind.Text = "Disconnected from: " + a.Device.Name.ToString();
-                listofdevices.IsVisible = true;
-                sendDataToBT.IsVisible = false;
-            };
+            
         }
 
         private void FTCShow_Clicked(object sender, EventArgs e)
@@ -77,39 +41,10 @@ namespace LightScout
         {
             Navigation.PushAsync(new FRCMain());
         }
-        private async void CheckBluetooth(object sender, EventArgs e)
-        {
-            //BindingContext = new BluetoothDeviceViewModel();
-            
-            Devices.Clear();
-            
-            await adapter.StartScanningForDevicesAsync();
-            
-        }
 
-        private async void listofdevices_ItemTapped(object sender, ItemTappedEventArgs e)
+        private void commscheck_Clicked(object sender, EventArgs e)
         {
-            IDevice selectedDevice = e.Item as IDevice;
-            
-            if(deviceIWant != null)
-            {
-                await adapter.DisconnectDeviceAsync(deviceIWant);
-                await adapter.ConnectToDeviceAsync(selectedDevice);
-            }
-            else
-            {
-                await adapter.ConnectToDeviceAsync(selectedDevice);
-            }
-        }
-
-        private async void sendDataToBT_Clicked(object sender, EventArgs e)
-        {
-            var servicetosend = await  deviceIWant.GetServiceAsync(Guid.Parse("50dae772-d8aa-4378-9602-792b3e4c198d"));
-            var characteristictosend = await servicetosend.GetCharacteristicAsync(Guid.Parse("50dae772-d8aa-4378-9602-792b3e4c198e"));
-            var stringtoconvert = "Hi!";
-            var bytestotransmit = Encoding.ASCII.GetBytes(stringtoconvert);
-            await characteristictosend.WriteAsync(bytestotransmit);
-            Console.WriteLine(bytestotransmit);
+            Navigation.PushAsync(new CommsCheck());
         }
     }
 }
--- LightScout/LightScout/obj/Debug/netstandard2.0/MainPage.xaml.g.cs
+++ LightScout/LightScout/obj/Debug/netstandard2.0/MainPage.xaml.g.cs
@@ -23,22 +23,14 @@ namespace LightScout {
         private global::Xamarin.Forms.Button FRCShow;
         
         [global::System.CodeDom.Compiler.GeneratedCodeAttribute("Xamarin.Forms.Build.Tasks.XamlG", "2.0.0.0")]
-        private global::Xamarin.Forms.Button BluetoothFind;
-        
-        [global::System.CodeDom.Compiler.GeneratedCodeAttribute("Xamarin.Forms.Build.Tasks.XamlG", "2.0.0.0")]
-        private global::Xamarin.Forms.ListView listofdevices;
-        
-        [global::System.CodeDom.Compiler.GeneratedCodeAttribute("Xamarin.Forms.Build.Tasks.XamlG", "2.0.0.0")]
-        private global::Xamarin.Forms.Button sendDataToBT;
+        private global::Xamarin.Forms.Button commscheck;
         
         [global::System.CodeDom.Compiler.GeneratedCodeAttribute("Xamarin.Forms.Build.Tasks.XamlG", "2.0.0.0")]
         private void InitializeComponent() {
             global::Xamarin.Forms.Xaml.Extensions.LoadFromXaml(this, typeof(MainPage));
             FTCShow = global::Xamarin.Forms.NameScopeExtensions.FindByName<global::Xamarin.Forms.Button>(this, "FTCShow");
             FRCShow = global::Xamarin.Forms.NameScopeExtensions.FindByName<global::Xamarin.Forms.Button>(this, "FRCShow");
-            BluetoothFind = global::Xamarin.Forms.NameScopeExtensions.FindByName<global::Xamarin.Forms.Button>(this, "BluetoothFind");
-            listofdevices = global::Xamarin.Forms.NameScopeExtensions.FindByName<global::Xamarin.Forms.ListView>(this, "listofdevices");
-            sendDataToBT = global::Xamarin.Forms.NameScopeExtensions.FindByName<global::Xamarin.Forms.Button>(this, "sendDataToBT");
+            commscheck = global::Xamarin.Forms.NameScopeExtensions.FindByName<global::Xamarin.Forms.Button>(this, "commscheck");
         }
     }
 }

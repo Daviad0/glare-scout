2d790efebda0e02360c568951f5da1400d309a2c
David Reeves <djreeves2005@gmail.com>
2020-07-20 10:13:46 -0400

    Code for USB

---
+++ LightScout/LightScout.iOS/USBTransfer.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+
+using Foundation;
+using LightScout.Models;
+using UIKit;
+
+[assembly: Xamarin.Forms.Dependency(typeof(LightScout.iOS.USBTransfer))]
+namespace LightScout.iOS
+{
+    public class USBTransfer : USBCommunication
+    {
+        public void SendData(string rawstring)
+        {
+            var socket = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+            socket.Bind(new IPEndPoint(IPAddress.Loopback, 862));
+
+            socket.Listen(100);
+            socket.BeginSend(Encoding.ASCII.GetBytes(rawstring), 0, Encoding.ASCII.GetBytes(rawstring).Length, SocketFlags.None, USBCallBack, new LSConfiguration());
+        }
+        public void USBCallBack(IAsyncResult result)
+        {
+
+        }
+    }
+}
---
+++ LightScout/LightScout/USBCommunication.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LightScout
+{
+    public interface USBCommunication
+    {
+        void SendData(string data);
+    }
+}

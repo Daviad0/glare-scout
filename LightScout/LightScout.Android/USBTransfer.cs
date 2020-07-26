c91766a77f7ffbdc940d42b6db153b7081a2bc7e
David Reeves <djreeves2005@gmail.com>
2020-07-26 16:19:41 -0400

    Simple usbManager test

---
+++ LightScout/LightScout.Android/USBTransfer.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using Android.App;
+using Android.Content;
+using Android.Hardware.Usb;
+using Android.OS;
+using Android.Runtime;
+using Android.Views;
+using Android.Widget;
+using Hoho.Android.UsbSerial.Driver;
+using Hoho.Android.UsbSerial.Util;
+
+[assembly: Xamarin.Forms.Dependency(typeof(LightScout.Droid.USBTransfer))]
+namespace LightScout.Droid
+{
+    public class USBTransfer : USBCommunication
+    {
+        IUsbSerialPort port;
+
+        SerialInputOutputManager serialIoManager;
+
+        UsbManager usbManager;
+        public void SendData(string data)
+        {
+            var usbList = usbManager.DeviceList;
+            foreach(var item in usbList)
+            {
+                Console.WriteLine(item.Key);
+            }
+        }
+    }
+}

05d5fdf8298c5bccdfbd811ec3ad31d1dac12dc5
David Reeves <djreeves2005@gmail.com>
2020-09-13 19:16:57 -0400

    no init?

--- LightScout/LightScout.iOS/AppDelegate.cs
+++ LightScout/LightScout.iOS/AppDelegate.cs
@@ -22,7 +22,7 @@ namespace LightScout.iOS
         //
         public override bool FinishedLaunching(UIApplication app, NSDictionary options)
         {
-            global::Xamarin.Forms.Forms.Init();
+            //global::Xamarin.Forms.Forms.Init();
             LoadApplication(new App());
             ZXing.Net.Mobile.Forms.iOS.Platform.Init();
             UIApplication.SharedApplication.StatusBarHidden = true;

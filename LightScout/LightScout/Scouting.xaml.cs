b3fa190ab6ea51f4fcf6cf56572f48ebcbe791c6
David Reeves <djreeves2005@gmail.com>
2021-06-09 17:25:20 -0400

    better design & performance; correct UI generation; work on different collection types in schema

--- LightScout/LightScout/Scouting.xaml.cs
+++ LightScout/LightScout/Scouting.xaml.cs
@@ -14,11 +14,13 @@ namespace LightScout
     {
         private bool optionsExpanded = false;
         private bool disabled = false;
+        public ColorTypeConverter converter = new ColorTypeConverter();
         private DateTime startDisabled = DateTime.Now;
         private TimeSpan totalDisabled = TimeSpan.FromSeconds(0);
         private DateTime startForm = DateTime.Now;
         public List<string> submitMethod = new List<string>();
         // could change dynamic to an object technically
+        public Dictionary<string, object> categoryGrids = new Dictionary<string, object>();
         public Dictionary<string, object> fields = new Dictionary<string, object>();
         public Dictionary<string, StackLayout> dynamicLayouts = new Dictionary<string, StackLayout>();
         public dynamic formObject = JObject.Parse(@"{
@@ -100,7 +102,7 @@ namespace LightScout
                 //Console.WriteLine(content.uniqueId.ToString());
                 if(content.type != "parent")
                 {
-                    Label addContent = new Label() { Text = content.prettyName.ToString(), HorizontalOptions = LayoutOptions.Center };
+                    Label addContent = new Label() { Text = content.prettyName.ToString(), Margin = new Thickness(0, 5, 0, 0), FontSize=18, HorizontalOptions = LayoutOptions.Center, TextColor = (Color)converter.ConvertFromInvariantString("#0F3F8C") };
                     ((StackLayout)dynamicLayouts[starter.uniqueId.ToString()]).Children.Add(addContent);
                     // remember submission order and add to specific dictionary based on the content needed
                     submitMethod.Add(content.uniqueId.ToString());
@@ -110,7 +112,7 @@ namespace LightScout
                 {
                     // add stack to parent, and then create another object for the next iteration
                     StackLayout newParent = new StackLayout() { ClassId = content.uniqueId };
-                    Label parentLabel = new Label() { Text = content.prettyName, FontSize = 20, FontAttributes = FontAttributes.Bold, HorizontalOptions = LayoutOptions.Center };
+                    Label parentLabel = new Label() { Text = content.prettyName, Margin = new Thickness(0, 5, 0, 0), FontSize = 20, FontAttributes = FontAttributes.Bold, HorizontalOptions = LayoutOptions.Center, TextColor = (Color)converter.ConvertFromInvariantString("#2A7AFA") };
                     newParent.Children.Add(parentLabel);
                     ((StackLayout)dynamicLayouts[starter.uniqueId.ToString()]).Children.Add(newParent);
                     dynamicLayouts.Add(content.uniqueId.ToString(), newParent);
@@ -125,9 +127,26 @@ namespace LightScout
             foreach(var category in formObject.categories)
             {
                 StackLayout newElement = new StackLayout() { ClassId = category.uniqueId };
-                Label categoryLabel = new Label() { Text = category.prettyName, FontSize = 24, FontAttributes = FontAttributes.Bold, HorizontalOptions = LayoutOptions.Center };
+                // create top grid or label depending on if it's needed due to "expectedTime" property
+                if(category.expectedStart != null)
+                {
+                    Grid categoryHeader = new Grid() { ColumnDefinitions = { new ColumnDefinition { Width = GridLength.Auto }, new ColumnDefinition { Width = GridLength.Auto } }, Margin = new Thickness(0, 30, 0, 0), HorizontalOptions = LayoutOptions.Center };
+                    Label categoryLabel = new Label() { Text = category.prettyName, FontSize = 28, FontAttributes = FontAttributes.Bold, HorizontalOptions = LayoutOptions.Center, TextColor = (Color)converter.ConvertFromInvariantString("#0F3F8C") };
+                    Frame categoryStartFrame = new Frame() { CornerRadius = 8, Padding = new Thickness(8, 4), HorizontalOptions = LayoutOptions.Start, BackgroundColor = (Color)converter.ConvertFromInvariantString("#0F3F8C"), VerticalOptions = LayoutOptions.Center, Margin = new Thickness(3, 0) };
+                    Label categoryStartLabel = new Label() { Text = "Start", TextColor = (Color)converter.ConvertFromInvariantString("Color.White"), FontSize = 18, HorizontalOptions = LayoutOptions.End, VerticalOptions = LayoutOptions.Center };
+                    categoryStartFrame.Content = categoryStartLabel;
+                    categoryHeader.Children.Add(categoryLabel, 0, 0);
+                    categoryHeader.Children.Add(categoryStartFrame, 1, 0);
+                    newElement.Children.Add(categoryHeader);
+                }
+                else
+                {
+                    Label categoryLabel = new Label() { Text = category.prettyName, FontSize = 28, FontAttributes = FontAttributes.Bold, HorizontalOptions = LayoutOptions.Center, TextColor = (Color)converter.ConvertFromInvariantString("#0F3F8C"), Margin = new Thickness(0, 30, 0, 0) };
+                    newElement.Children.Add(categoryLabel);
+                }
+                
                 dynamicLayouts.Add(category.uniqueId.ToString(), newElement);
-                newElement.Children.Add(categoryLabel);
+                
                 mainParent.Children.Add(newElement);
                 // recursion until all of the separate items are finally added to the Dictionary and list
                 await deeperSchemaLevel(category);
@@ -137,31 +156,54 @@ namespace LightScout
         }
         private async void exitPhase(object sender, EventArgs e)
         {
-            Navigation.PushAsync(new MasterPage());
+            bool ans = await DisplayAlert("Are you sure?", "Do you want to leave this form? Your changes will not be saved!", "Exit", "Cancel");
+            if (ans)
+            {
+                Navigation.PushAsync(new MasterPage());
+            }
+            else
+            {
+                // we know that options IS expanded, so we can contract it
+                expandOptions(null, null);
+            }
+            
         }
         private async void toggleDisabled(object sender, EventArgs e)
         {
             if (disabled)
             {
                 elapsedContainer.TranslateTo(0, 0, 500, Easing.CubicInOut);
-                disabledMenu.TranslateTo(0, disabledMenu.Height+8, 500, Easing.CubicInOut);
+                disabledMenu.TranslateTo(0, disabledMenu.Height+10, 500, Easing.CubicInOut);
                 disabled = false;
                 totalDisabled = DateTime.Now - startDisabled + totalDisabled;
+                //elapsedFrame2.FadeTo(0, 250, Easing.CubicInOut);
+                // we know that options IS expanded, so we can contract it
+                if (optionsExpanded)
+                {
+                    expandOptions(null, null);
+                }   
             }
             else
             {
-                elapsedContainer.TranslateTo(0, Math.Round((disabledMenu.Height * -1) + 8), 500, Easing.CubicInOut);
+                elapsedContainer.TranslateTo(0, Math.Round((disabledMenu.Height * -1) + 10), 500, Easing.CubicInOut);
                 disabledMenu.TranslateTo(0, 8, 500, Easing.CubicInOut);
                 startDisabled = DateTime.Now;
                 disabled = true;
+                //elapsedFrame2.FadeTo(1, 250, Easing.CubicInOut);
+                expandOptions(null, null);
             }
+        }
+        private async void resetDisabled(object sender, EventArgs e)
+        {
+            startDisabled = DateTime.Now;
+            totalDisabled = TimeSpan.FromSeconds(0);
         }
         private async void expandOptions(object sender, EventArgs e)
         {
 
             Action<double> heicallback = input => { options.Height = input; };
             Action<double> corcallback = input => { optionsBar.CornerRadius = (float)input; };
-            var startingHeight = optionsExpanded ? 150 : 0;
+            var startingHeight = optionsExpanded ? 160 : 0;
             // ensure that no overflow content is showed
             
 
@@ -189,7 +231,7 @@ namespace LightScout
             else
             {
                 optionsExpanded = true;
-                var endingHeight = 150;
+                var endingHeight = 160;
                 var heianim = new Animation(heicallback, startingHeight, endingHeight, Easing.CubicInOut);
                 var coranim = new Animation(corcallback, 0, 8, Easing.CubicInOut);
 
@@ -217,12 +259,13 @@ namespace LightScout
         {
             Device.StartTimer(TimeSpan.FromMilliseconds(20), () =>
             {
-                elapsed.Text = Math.Floor((DateTime.Now - startForm).TotalMinutes).ToString() + ":" + ((Math.Floor((DateTime.Now - startForm).TotalSeconds) - (Math.Floor((DateTime.Now - startForm).TotalMinutes) * 60)).ToString().Length == 1 ? "0" : "") + (Math.Floor((DateTime.Now - startForm).TotalSeconds)- (Math.Floor((DateTime.Now - startForm).TotalMinutes)*60)).ToString();
+                elapsed1.Text = Math.Floor((DateTime.Now - startForm).TotalMinutes).ToString() + ":" + ((Math.Floor((DateTime.Now - startForm).TotalSeconds) - (Math.Floor((DateTime.Now - startForm).TotalMinutes) * 60)).ToString().Length == 1 ? "0" : "") + (Math.Floor((DateTime.Now - startForm).TotalSeconds)- (Math.Floor((DateTime.Now - startForm).TotalMinutes)*60)).ToString();
+                //elapsed2.Text = Math.Floor((DateTime.Now - startForm).TotalMinutes).ToString() + ":" + ((Math.Floor((DateTime.Now - startForm).TotalSeconds) - (Math.Floor((DateTime.Now - startForm).TotalMinutes) * 60)).ToString().Length == 1 ? "0" : "") + (Math.Floor((DateTime.Now - startForm).TotalSeconds)- (Math.Floor((DateTime.Now - startForm).TotalMinutes)*60)).ToString();
                 if (disabled)
                 {
                     var totalMinutes = Math.Floor((totalDisabled + (DateTime.Now - startDisabled)).TotalMinutes);
                     var totalSeconds = Math.Floor((totalDisabled + (DateTime.Now - startDisabled)).TotalSeconds - (totalMinutes*60));
-                    disabledElapsed.Text = "Disabled for " + totalMinutes.ToString() + ":" + (totalSeconds.ToString().Length == 1 ? "0" : "") + totalSeconds.ToString();
+                    disabledElapsed.Text = totalMinutes.ToString() + ":" + (totalSeconds.ToString().Length == 1 ? "0" : "") + totalSeconds.ToString();
                 }
                 return true;
             });
